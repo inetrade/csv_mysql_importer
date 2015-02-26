97f76392fbe8d7db445ed998bb94908b59b395ba
Philipp Metzler <pm@killphil.prg>
2015-02-26 21:29:53 +0100

    added exception and closed db connection

--- importer.py
+++ importer.py
@@ -3,36 +3,45 @@ import mysql.connector
 
 # data format example:
 # "2348";"Meyer Hans";;"0049123456567";
+source_csv = 'some_file.csv'
 
 # database connection
 cnx = mysql.connector.connect(user='root', database='geheim')
 cursor = cnx.cursor()
 
-source_csv = 'some_file.csv'
-
 with open(source_csv, newline='') as csvfile:
     csv_data = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in csv_data:
+        try:
 
-        # manipulate data
-        short_code = row[0]
-        name = row[1]
-        some_field = row[2]
-        phone_number = row[3]
+            # manipulate data
+            short_code = row[0]
+            name = row[1]
+            some_field = row[2]
+            phone_number = row[3]
 
-        query = ("""
-                    INSERT INTO tablename
-                        (short_code, name, some_field, phone_number)
-                    VALUES 
-                        (%s, %s, %s, %s) 
-                    ON DUPLICATE KEY UPDATE 
-                                            -- no need to update the PK
-                    name = VALUES(%s), 
-                    some_field = VALUES(%s), 
-                    phone_number = VALUES(%s) ;
-                    """
-                    )
-        data = (short_code, name, some_field, phone_number,
-                name, some_field, phone_number,)
-        cursor.execute(query, data)
+            query = ("""
+                        INSERT INTO tablename
+                            (short_code, name, some_field, phone_number)
+                        VALUES 
+                            (%s, %s, %s, %s) 
+                        ON DUPLICATE KEY UPDATE 
+                                                -- no need to update the PK
+                        name = VALUES(%s), 
+                        some_field = VALUES(%s), 
+                        phone_number = VALUES(%s) ;
+                        """
+                        )
+
+            data = (short_code, name, some_field, phone_number,
+                    name, some_field, phone_number,)
+            cursor.execute(query, data)
+            cursor.close()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            cnx.rollback()
+
+    cnx.commit()
+cnx.close()
 
